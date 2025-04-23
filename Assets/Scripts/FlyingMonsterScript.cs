0c50fbc5a5c1e6624b3461ce618e192e29f73d69
GitStomp <alex.lara@cox.net>
2025-04-23 13:46:04 -0700

    made more stuff

--- Assets/Scripts/FlyingMonsterScript.cs
+++ Assets/Scripts/FlyingMonsterScript.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RubieScript : MonoBehaviour
+public class FlyingMonsterScript : MonoBehaviour
 {
-    public int rubieValue;
-
     // Start is called before the first frame update
     void Start()
     {
