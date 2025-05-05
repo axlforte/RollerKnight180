0a5efe732f197c6a23ca675be2702e9f194b812f
axlforte <wisarddlwthecoder@gmail.com>
2025-05-05 12:43:14 -0700

    added counter system for interactibles

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,13 +7,17 @@ public class Enemy : MonoBehaviour
     public int health = 1;
     public float invTime = 8;// the invuln needs to be just long enough to not get double hit by a sword swing
     public int damage = 1;//how many quarters of a heart the enemy removes with contact
-    
+    public Interactible interactible;//only for enemies that contribute to a counter based system. 
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.layer == 6)
         {
             health--;
+            if(interactible != null)
+            {
+                interactible.pinged = true;
+            }
         }
     }
 }
